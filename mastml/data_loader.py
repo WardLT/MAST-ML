2eb7b5d09579d4047a8db33bc304ab8448c16924
Lane <love.allsciences@gmail.com>
2018-10-04 11:51:50 -0500

    pep8 changes to data_loader.py

--- mastml/data_loader.py
+++ mastml/data_loader.py
@@ -4,24 +4,41 @@ Module for loading checking the input data file
 
 import pandas as pd
 import logging
+
 log = logging.getLogger('mastml')
 
-def load_data(file_path, input_features=None, target_feature=None, grouping_feature = None, feature_blacklist=list()):
-    " Loads in csv from filename and ensures required columns are present. Returns dataframe. "
+
+def load_data(
+              file_path,
+              input_features=None,
+              target_feature=None,
+              grouping_feature=None,
+              feature_blacklist=list()
+              ):
+
+    '''
+    Loads in csv from filename and ensures required columns are present.
+    Returns dataframe.
+    '''
 
     # Load data
     try:
         df = pd.read_csv(file_path)
-    except:
+    except Exception:
         df = pd.read_excel(file_path)
 
     # Assign default values to input_features and target_feature;
-    if input_features is None and target_feature is None: # input is first n-1 and target is just n
+    # input is first n-1 and target is just n
+    if input_features is None and target_feature is None:
         input_features = list(df.columns[:-1])
         target_feature = df.columns[-1]
-    elif input_features is None: # input is all the features except the target feature
+
+    # input is all the features except the target feature
+    elif input_features is None:
         input_features = [col for col in df.columns if col != target_feature]
-    elif target_feature is None: # target is the last non-input feature
+
+    # target is the last non-input feature
+    elif target_feature is None:
         for col in df.columns[::-1]:
             if col not in input_features:
                 target_feature = col
@@ -37,8 +54,12 @@ def load_data(file_path, input_features=None, target_feature=None, grouping_feat
 
     X, y = df[input_features], df[target_feature]
 
-    log.info('blacklisted features, either from "not_input_features" or a "grouping_column":' +
-                 str(feature_blacklist))
+    log.info(
+             'blacklisted features, either from ' +
+             '"not_input_features" or a "grouping_column":' +
+             str(feature_blacklist)
+             )
+
     # take blacklisted features out of X:
     X_noinput_dict = dict()
     for feature in set(feature_blacklist):
@@ -49,6 +70,7 @@ def load_data(file_path, input_features=None, target_feature=None, grouping_feat
 
     if grouping_feature:
         X_grouped = pd.DataFrame(df[grouping_feature])
+
     else:
         X_grouped = None
 
